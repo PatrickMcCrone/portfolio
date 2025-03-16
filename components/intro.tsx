4a51bfde68f866a54f48266c2ba38984bea795bb
PatrickMcCrone <pjmccrone@outlook.com>
2025-03-16 19:21:57 -0400

    Add Skills and Experience sections

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use } from "react";
+import React from "react";
 import Image from "next/image";
 import PeejSwimHeadshot from "@/public/PeejSwimHeadshot.jpg";
 import { motion } from "framer-motion";
@@ -8,118 +8,125 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Intro() {
-    return (
-        <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
-            <div className="flex items-center justify-center">
-                <div className="relative">
-                    <motion.div
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{ type: "tween", duration: 0.2 }}
-                    >
-                        <Image
-                            src={PeejSwimHeadshot}
-                            alt="Patrick Portrait"
-                            width="192"
-                            height="192"
-                            quality="95"
-                            priority={true}
-                            className="h-24 w-24 rounded-full object-cover 
+	const { ref } = useSectionInView("Home", 0.5);
+
+	return (
+		<section
+			ref={ref}
+			id="home"
+			className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+		>
+			<div className="flex items-center justify-center">
+				<div className="relative">
+					<motion.div
+						initial={{ opacity: 0, scale: 0 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{ type: "tween", duration: 0.2 }}
+					>
+						<Image
+							src={PeejSwimHeadshot}
+							alt="Patrick Portrait"
+							width="192"
+							height="192"
+							quality="95"
+							priority={true}
+							className="h-24 w-24 rounded-full object-cover 
                     border-[0.35rem] border-white shadow-xl"
-                        />
-                    </motion.div>
-                    <motion.span
-                        className="absolute bottom-0 right-0 text-4xl"
-                        initial={{ opacity: 0, scale: 0 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            type: "spring",
-                            stiffness: 125,
-                            delay: 0.1,
-                            duration: 0.7,
-                        }}
-                    >
-                        👋
-                    </motion.span>
-                </div>
-            </div>
+						/>
+					</motion.div>
+					<motion.span
+						className="absolute bottom-0 right-0 text-4xl"
+						initial={{ opacity: 0, scale: 0 }}
+						animate={{ opacity: 1, scale: 1 }}
+						transition={{
+							type: "spring",
+							stiffness: 125,
+							delay: 0.1,
+							duration: 0.7,
+						}}
+					>
+						👋
+					</motion.span>
+				</div>
+			</div>
 
-            <motion.h1
-                className="mb-10 mt-4 px-4 text-2xl font-medium
+			<motion.h1
+				className="mb-10 mt-4 px-4 text-2xl font-medium
             !leading-[1.5] sm:text-4xl"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-            >
-                <span className="font-bold">Hello, I'm Patrick.</span> I'm a{" "}
-                <span className="font-bold">
-                    computer science student (Class of 2025)
-                </span>{" "}
-                at <span className="font-bold">SUNY Fredonia</span>. I enjoy
-                building{" "}
-                <span className="italic">all types of applications</span>. My
-                focuses are{" "}
-                <span className="underline">
-                    Python, C# (.NET), and Web Development
-                </span>
-                .
-            </motion.h1>
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+			>
+				<span className="font-bold">Hello, I'm Patrick.</span> I'm a{" "}
+				<span className="font-bold">
+					computer science student (Class of 2025)
+				</span>{" "}
+				at <span className="font-bold">SUNY Fredonia</span>. I enjoy
+				building{" "}
+				<span className="italic">all types of applications</span>. My
+				focuses are{" "}
+				<span className="underline">
+					Python, C# (.NET), and Web Development
+				</span>
+				.
+			</motion.h1>
 
-            <motion.div
-                className="flex flex-col sm:flex-row 
+			<motion.div
+				className="flex flex-col sm:flex-row 
         items-center justify-center gap-2 px-4 text-lg font-medium"
-                initial={{ opacity: 0, y: 100 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.1 }}
-            >
-                <Link
-                    href="#contact"
-                    className="group bg-gray-900 text-white px-7 py-3
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ delay: 0.1 }}
+			>
+				<Link
+					href="#contact"
+					className="group bg-gray-900 text-white px-7 py-3
                 flex items-center gap-2 rounded-full outline-none
                 focus:scale-110 hover:scale-110 hover:bg-gray-950
                 active:scale-105 transition"
-                >
-                    Contact Me Here{" "}
-                    <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
-                </Link>
+				>
+					Contact Me Here{" "}
+					<BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+				</Link>
 
-                <a
-                    className="group bg-white px-7 py-3
+				<a
+					className="group bg-white px-7 py-3
             flex items-center gap-2 rounded-full
             outline-none focus:scale-110 hover:scale-110 
             active:scale-105 transition cursor-pointer
             border border-black/10"
-                    href="/Resume.pdf"
-                    download={true}
-                >
-                    Download Resume{" "}
-                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-                </a>
+					href="/Resume.pdf"
+					download={true}
+				>
+					Download Resume{" "}
+					<HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+				</a>
 
-                <a
-                    className="bg-white p-4 text-gray-700
+				<a
+					className="bg-white p-4 text-gray-700
             flex items-center gap-2 rounded-full 
             focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950
             active:scale-105 transition cursor-pointer border border-black/10"
-                    href="https://www.linkedin.com/in/patrick-mccrone-77b92028b/"
-                    target="_blank"
-                >
-                    <BsLinkedin />
-                </a>
+					href="https://www.linkedin.com/in/patrick-mccrone-77b92028b/"
+					target="_blank"
+				>
+					<BsLinkedin />
+				</a>
 
-                <a
-                    className="bg-white p-4 text-gray-700
+				<a
+					className="bg-white p-4 text-gray-700
             flex items-center gap-2 text-[1.35rem]
             rounded-full focus:scale-[1.15] hover:scale-[1.15] 
             hover:text-gray-950 active:scale-105 transition cursor-pointer
             border border-black/10"
-                    href="https://github.com/PatrickMcCrone/"
-                    target="_blank"
-                >
-                    <FaGithubSquare />
-                </a>
-            </motion.div>
-        </section>
-    );
+					href="https://github.com/PatrickMcCrone/"
+					target="_blank"
+				>
+					<FaGithubSquare />
+				</a>
+			</motion.div>
+		</section>
+	);
 }
--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -1,7 +1,48 @@
-import React, { useState } from "react";
+"use client";
 
-export default function ActiveSectionContextProvider() {
-    const [activeSection, setActiveSection] = useState("Home");
+import type { SectionName } from "@/lib/types";
+import React, { useState, createContext, useContext } from "react";
 
-    return <div>ActiveSectionContextProvider</div>;
+type ActiveSectionContextProviderProps = {
+	children: React.ReactNode;
+};
+
+type ActiveSectionContextType = {
+	activeSection: SectionName;
+	setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+	timeOfLastClick: number;
+	setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const ActiveSectionContext =
+	createContext<ActiveSectionContextType | null>(null);
+
+export default function ActiveSectionContextProvider({
+	children,
+}: ActiveSectionContextProviderProps) {
+	const [activeSection, setActiveSection] = useState<SectionName>("Home");
+	const [timeOfLastClick, setTimeOfLastClick] = useState(0);
+
+	return (
+		<ActiveSectionContext.Provider
+			value={{
+				activeSection,
+				setActiveSection,
+				timeOfLastClick,
+				setTimeOfLastClick,
+			}}
+		>
+			{children}
+		</ActiveSectionContext.Provider>
+	);
+}
+
+export function useActiveSectionContext() {
+	const context = useContext(ActiveSectionContext);
+	if (context === null) {
+		throw new Error(
+			"useActiveSectionContext must be used within an ActiveSectionContextProvider"
+		);
+	}
+	return context;
 }
